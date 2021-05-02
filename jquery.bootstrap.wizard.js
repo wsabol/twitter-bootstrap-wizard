85e92e5aad3b1e7f454d75e40cf4dd41cc9ee158
Will Sabol <wsabol39@gmail.com>
2021-05-01 20:30:46 -0500

    basic pills

--- jquery.bootstrap.wizard.js
+++ jquery.bootstrap.wizard.js
@@ -331,7 +331,7 @@ $.fn.bootstrapWizard = function(options) {
 // expose options
 $.fn.bootstrapWizard.defaults = {
     withVisible:      true,
-    tabClass:         'nav nav-pills',
+    tabClass:         'nav nav-tabs',
     nextSelector:     '.wizard .next',
     previousSelector: '.wizard .previous',
     firstSelector:    '.wizard .first',
